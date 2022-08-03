537990edbe4d2056782c74c146b1991cda36a076
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2022-08-03 09:59:43 -0300

    versão inicial, sem código algum

---
+++ eShopCloudNative.Catalog.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace eShopCloudNative.Catalog.WebUI.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
