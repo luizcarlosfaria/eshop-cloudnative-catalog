fef4e84e430fb879142ce4af7c2fb59337bc06f8
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2022-08-05 05:56:01 -0300

    Criando o Image Controller (empty)

---
+++ eShopCloudNative.Catalog.WebUI/Controllers/ImageController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace eShopCloudNative.Catalog.WebUI.Controllers;
+public class ImageController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
