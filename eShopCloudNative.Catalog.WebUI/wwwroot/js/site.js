1d497b678d551b6c9f8a0e715de88ce00ac26d12
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2022-08-11 09:38:48 -0300

    Adição de app vue

--- eShopCloudNative.Catalog.WebUI/wwwroot/js/site.js
+++ eShopCloudNative.Catalog.WebUI/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Hello Vue!'
+        }
+    }
+}).mount('#full-page')
+
+ 
