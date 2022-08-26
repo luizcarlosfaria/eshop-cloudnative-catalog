d66154b33ef20fa589ef00473b8d7f4446f7ec66
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2022-08-25 22:32:49 -0300

    Syncronização dos DTO's

---
+++ eShopCloudNative.Catalog.Contracts/Dto/CategoryTypeDto.cs
@@ -0,0 +1,16 @@
+﻿namespace eShopCloudNative.Catalog.Dto;
+
+public class CategoryTypeDto
+{
+    public int CategoryTypeId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public IList<CategoryDto>? Categories { get; set; }
+
+    public bool? ShowOnMenu { get; set; }
+
+    public bool? IsHomeShowCase { get; set; }
+}
--- eShopCloudNative.Catalog.Contracts/Dto/ProductDto.cs
+++ eShopCloudNative.Catalog.Contracts/Dto/ProductDto.cs
@@ -4,9 +4,9 @@ public class ProductDto
 {
     public int ProductId { get; set; }
     public string? Name { get; set; }
+    public IList<CategoryDto>? Categories { get; set; }
     public string? Description { get; set; }
-    public string? Slug { get; set; }    
-    public IList<ImageDto>? Images { get; set; }
-    
-    public decimal? Price { get; set; }
+    public string? Slug { get; set; }
+    public decimal Price { get; set; }
+    public bool Active { get; set; }
 }
