37bc770b4a6fe3f9f30931576f9410eb4d471484
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2022-09-01 01:07:24 -0300

    Removendo criação de sample data na web api (agora é realizado completamente pelo boostrapper)

--- eShopCloudNative.Catalog.WebApi/Program.cs
+++ eShopCloudNative.Catalog.WebApi/Program.cs
@@ -1,4 +1,4 @@
-using eShopCloudNative.Catalog.Architecture.Data;
+    using eShopCloudNative.Catalog.Architecture.Data;
 using eShopCloudNative.Catalog.Entities;
 using eShopCloudNative.Catalog.Services;
 using FluentNHibernate.Cfg;
@@ -16,7 +16,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddNHibernate<CategoryMapping>(Constants.Schema, "catalog");
+builder.Services.AddNHibernate(cfg => cfg
+    .Schema(Constants.Schema)
+    .ConnectionStringKey("catalog")
+    .AddMappingsFromAssemblyOf<CategoryMapping>()
+    .RegisterSession()
+    .RegisterStatelessSession()
+    );
 
 builder.Services.AddScoped<IProductService, ProductService>();
 
@@ -37,41 +43,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-
-var session = app.Services.GetRequiredService<ISessionFactory>().OpenSession();
-
-var categoryType = session.Query<CategoryType>().Where(it=> it.CategoryTypeId == 1).SingleOrDefault();
-if (categoryType == null)
-{
-    categoryType = new CategoryType() { CategoryTypeId = 1, Name = "CategoryTypeId 1", IsHomeShowCase = true, ShowOnMenu = false };
-    session.Save(categoryType);
-}
-
-
-var item = session.Query<Category>().Where(it=> it.CategoryId == 1).SingleOrDefault();
-if (item != null)
-{
-    Console.WriteLine($"Root {item.CategoryId}");
-    Console.WriteLine($"BEGIN Children of {item.CategoryId}");
-    if (item.Children != null)
-        foreach (var child in item.Children)
-        {
-            Console.WriteLine($"    Child {child.CategoryId}");
-        }
-    Console.WriteLine($"END Children of {item.CategoryId}");
-}
-
-var categoryToCreate = new Category() { Name = "Category Teste 1", Slug = "teste", Type = categoryType, Active = true, Description = "" };
-var productToCreate = new Product() { Name = "Product Teste 1", Slug = "teste", Active = true, Description = "", Price = 5, Categories = new List<Category>() {  } };
-
-if (item != null && item.Children != null && item.Children.Count > 0)
-{
-    productToCreate.Categories.Add(item.Children.First());
-}
-
-
-session.Save(categoryToCreate);
-session.Save(productToCreate);
-session.Flush();
 
 app.Run();
