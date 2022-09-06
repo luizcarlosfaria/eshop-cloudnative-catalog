a4013227aaff07feb106379b75d89ad9ca8d2a37
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2022-09-05 23:34:08 -0300

    Quebra de linha

--- eShopCloudNative.Catalog.WebApi/Controllers/PublicCatalogController.cs
+++ eShopCloudNative.Catalog.WebApi/Controllers/PublicCatalogController.cs
@@ -15,8 +15,10 @@ public class PublicCatalogController : ControllerBase, IPublicCatalogService
     }
 
     [HttpGet("CategoriesForMenu", Name = "CategoriesForMenu")]
-    public async Task<IEnumerable<CategoryDto>> GetCategoriesForMenu() => await this.categoryService.GetCategoriesForMenu();
+    public async Task<IEnumerable<CategoryDto>> GetCategoriesForMenu() 
+        => await this.categoryService.GetCategoriesForMenu();
 
     [HttpGet("HomeCatalog", Name = "HomeCatalog")]
-    public async Task<IEnumerable<CategoryDto>> GetHomeCatalog() => await this.categoryService.GetHomeCatalog();
+    public async Task<IEnumerable<CategoryDto>> GetHomeCatalog() 
+        => await this.categoryService.GetHomeCatalog();
 }
