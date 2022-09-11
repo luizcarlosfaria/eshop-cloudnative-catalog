392fb7ff2a3779963232aa0937e278289147e039
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2022-09-11 04:49:26 -0300

    Movendo PostgresBootstrapperService.cs para eShopCloudNative.Architecture.Bootstrap

--- eShopCloudNative.Catalog.Bootstrapper/Bootstrapper/Program.cs
+++ eShopCloudNative.Catalog.Bootstrapper/Bootstrapper/Program.cs
@@ -1,12 +1,11 @@
 ﻿using eShopCloudNative.Architecture.Bootstrap;
 using eShopCloudNative.Architecture.Extensions;
+using eShopCloudNative.Catalog.Architecture.Data;
 using eShopCloudNative.Catalog.Bootstrapper;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Spring.Context.Support;
-
-
 
 
 var configuration = new ConfigurationBuilder()
@@ -19,6 +18,7 @@ var configuration = new ConfigurationBuilder()
 
 XmlApplicationContext context = new CodeConfigApplicationContext()
         .RegisterInstance("Configuration", configuration)
+        .RegisterInstance("DatabaseSchema", CatalogConstants.Schema)
         .CreateChildContext("./bootstrapper.xml");
 
 var bootstrapperService = context.GetObject<BootstrapperService>("BootstrapperService");
