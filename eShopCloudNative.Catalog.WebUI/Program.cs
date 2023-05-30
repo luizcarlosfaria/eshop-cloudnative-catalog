a92e5bc1e7d6fce89f6c414acaf86264eec3d563
Luiz Carlos Faria <luizcarlosfaria@gmail.com>
2023-05-30 19:37:40 -0300

    Code Analysis

--- eShopCloudNative.Catalog.WebUI/Program.cs
+++ eShopCloudNative.Catalog.WebUI/Program.cs
@@ -129,23 +129,3 @@ app.MapControllerRoute(
     pattern: "{controller=Catalog}/{action=Index}/{id?}");
 
 app.Run();
-
-
-public class RequestDelegatingHandler : DelegatingHandler
-{
-    protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
-    {
-        using (new EnterpriseApplicationLogContext(request.RequestUri.ToString(), request.Method.ToString(), it => it.AddProperty("Refit", true).AddProperty("Async", false)))
-        {
-            return base.Send(request, cancellationToken);
-        }
-    }
-
-    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-    {
-        using (new EnterpriseApplicationLogContext(request.RequestUri.ToString(), request.Method.ToString(), it => it.AddProperty("Refit", true).AddProperty("Async", true)))
-        {
-            return await base.SendAsync(request, cancellationToken);
-        }
-    }
-}
